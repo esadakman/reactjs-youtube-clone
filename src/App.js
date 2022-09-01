40b171a919c655f46a938c88304b5eb6ccf21a7d
esadakman <esadd26@gmail.com>
2022-09-01 13:34:45 +0300

    router & components

--- src/App.js
+++ src/App.js
@@ -1,9 +1,22 @@
-import React from 'react'
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Box } from "@mui/material";
+import { Feed } from "@mui/icons-material";
+import VideoDetail from "./components/VideoDetail";
+import ChannelDetail from "./components/ChannelDetail";
+import SearchFeed from "./components/SearchFeed";
 
 const App = () => {
-  return (
-    <div>App</div>
-  )
-}
+  <BrowserRouter>
+    <Box sx={{ backgroundColor: "#000" }}>
+      Navbar
+      <Routes>
+        <Route exact path="/" element={<Feed />} />
+        <Route path="/video/:id" element={<VideoDetail />} />
+        <Route path="/channel/:id" element={<ChannelDetail />} />
+        <Route path="/search/:searchTerm" element={<SearchFeed />} />
+      </Routes>
+    </Box>
+  </BrowserRouter>;
+};
 
-export default App
+export default App;
