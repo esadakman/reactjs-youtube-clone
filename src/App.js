af08898897d6931f5238253f8b3b2bd55c9a2a13
esadakman <esadd26@gmail.com>
2022-09-01 14:03:27 +0300

    Navbar & constants

--- src/App.js
+++ src/App.js
@@ -1,14 +1,18 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { Box } from "@mui/material";
-import { Feed } from "@mui/icons-material";
-import VideoDetail from "./components/VideoDetail";
-import ChannelDetail from "./components/ChannelDetail";
-import SearchFeed from "./components/SearchFeed";
 
-const App = () => {
+import {
+  ChannelDetail,
+  VideoDetail,
+  SearchFeed,
+  Navbar,
+  Feed,
+} from "./components";
+
+const App = () => (
   <BrowserRouter>
     <Box sx={{ backgroundColor: "#000" }}>
-      Navbar
+      <Navbar />
       <Routes>
         <Route exact path="/" element={<Feed />} />
         <Route path="/video/:id" element={<VideoDetail />} />
@@ -16,7 +20,7 @@ const App = () => {
         <Route path="/search/:searchTerm" element={<SearchFeed />} />
       </Routes>
     </Box>
-  </BrowserRouter>;
-};
+  </BrowserRouter>
+);
 
 export default App;
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,7 +1,28 @@
-import React from "react";
+import { Stack } from "@mui/material";
+import { Link } from "react-router-dom";
 
-const Navbar = () => {
-  return <div>Navbar</div>;
-};
+import { SearchBar } from "./";
+
+const Navbar = () => (
+  <Stack
+    direction="row"
+    alignItems="center"
+    pt={2}
+    sx={{
+      position: "sticky",
+      background: "#000",
+      top: 0,
+      justifyContent: "space-between",
+    }}
+  >
+    <Link
+      to="/"
+      style={{ display: "flex", alignItems: "center", marginLeft: "1rem" }}
+    >
+      <img src="/logo.png" alt="logo" height={45} />
+    </Link>
+    <SearchBar />
+  </Stack>
+);
 
 export default Navbar;
