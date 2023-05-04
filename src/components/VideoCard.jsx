d7c922fad5ac83708b2b2e60b93c92774adba23c
esadakman <esadd26@gmail.com>
2023-05-04 23:12:10 +0300

    css fixed

--- src/components/VideoCard.jsx
+++ src/components/VideoCard.jsx
@@ -18,7 +18,17 @@ const VideoCard = ({
   },
 }) => (
   <Card
-    sx={{ width: { xs: "100%", sm: "358px", md: "320px" }, boxShadow: "none", borderRadius: 0,}}
+    sx={{
+      width: "100%",
+      maxWidth: "358px",
+      boxShadow: "none",
+      borderRadius: 0,
+      margin: "0 1rem 1rem 0",
+      transition: "all 0.2s",
+      "&:hover": {
+        opacity: 0.7,
+      },
+    }}
   >
     <Link to={videoId ? `/video/${videoId}` : `/video/cV2gBU6hKfY`}>
       <CardMedia
@@ -27,16 +37,45 @@ const VideoCard = ({
         sx={{ width: { xs: "100%", sm: "358px" }, height: 180 }}
       />
     </Link>
-    <CardContent sx={{ backgroundColor: "#1E1E1E", height: "106px" }}>
-      <Link to={videoId ? `/video/${videoId}` : demoVideoUrl}>
-        <Typography variant="subtitle1" fontWeight="bold" color="#FFF">
+    <CardContent sx={{ backgroundColor: "#1E1E1E", height: "91px" }}>
+      <Link
+        to={videoId ? `/video/${videoId}` : demoVideoUrl}
+        style={{ textDecoration: "none" }}
+      >
+        <Typography
+          variant="subtitle1"
+          fontWeight="bold"
+          color="#FFF"
+          sx={{
+            overflow: "hidden",
+            textOverflow: "ellipsis",
+            display: "-webkit-box",
+            WebkitLineClamp: 2,
+            WebkitBoxOrient: "vertical",
+          }}
+        >
           {snippet?.title.slice(0, 60) || demoVideoTitle.slice(0, 60)}
         </Typography>
       </Link>
-      <Link to={snippet?.channelId ? `/channel/${snippet?.channelId}` : demoChannelUrl}>
-        <Typography variant="subtitle2" color="gray">
+      <Link
+        to={
+          snippet?.channelId ? `/channel/${snippet?.channelId}` : demoChannelUrl
+        }
+        style={{
+          textDecoration: "none",
+          display: "flex",
+          alignItems: "center",
+        }}
+      >
+        <Typography
+          variant="subtitle2"
+          color="gray"
+          sx={{ display: "flex", alignItems: "center" }}
+        >
           {snippet?.channelTitle || demoChannelTitle}
-          <CheckCircleIcon sx={{ fontSize: "12px", color: "gray", ml: "5px" }}/>
+          <CheckCircleIcon
+            sx={{ fontSize: "12px", color: "gray", ml: "5px" }}
+          />
         </Typography>
       </Link>
     </CardContent>
